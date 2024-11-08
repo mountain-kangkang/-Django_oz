f73f766e1598765226e8142b651b4f46617264eb
mountain-kangkang <rngl10@naver.com>
2024-11-08 15:15:42 +0900

    Django 01-6 간단한 HTTP 응답 만들어보기

--- bookmark/config/urls.py
+++ bookmark/config/urls.py
@@ -17,12 +17,28 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<h1>Hello, world.</h1>")
 
+def book_list(request):
+    book_text = ''
+    for i in range(0, 10):
+        book_text += f'book {i}<br>'
+
+    return HttpResponse(book_text)
+
+def book(request, num):
+    book_text = f'book {num}번 페이지입니다.'
+    return HttpResponse(book_text)
+
+def language(request, lang):
+    return HttpResponse(f'<h1>{lang} 언어 페이지입니다.</h1>')
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", index, name="index"),
+    path("book_list/", book_list, name="book_list"),
+    path("book_list/<int:num>/", book, name="book"),
+    path("language/<str:lang>/", language, name="language"),
 ]
