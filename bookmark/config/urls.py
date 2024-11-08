9602ce7592c08f2ed89efd90f98e559ba7c185d2
mountain-kangkang <rngl10@naver.com>
2024-11-08 17:04:37 +0900

    Django 01_07 가짜 DB 활용해보기

--- bookmark/config/urls.py
+++ bookmark/config/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 
 def index(request):
     return HttpResponse("<h1>Hello, world.</h1>")
@@ -29,16 +29,43 @@ def book_list(request):
 
     return HttpResponse(book_text)
 
+movie_list = [
+    {'title': '파묘', 'viewers': 11913725},
+    {'title': '범죄도시4', 'viewers': 11501621},
+    {'title': '인사이드 아웃 2', 'viewers': 8796146},
+    {'title': '베테랑2', 'viewers': 7522494},
+]
+
 def book(request, num):
     book_text = f'book {num}번 페이지입니다.'
     return HttpResponse(book_text)
 
 def language(request, lang):
     return HttpResponse(f'<h1>{lang} 언어 페이지입니다.</h1>')
+
+def python(request):
+    return HttpResponse('python 페이지 입니다.')
+
+def movies(request):
+    movie_titles = [
+        f'<a href="/movie/{index}/">{movie['title']}</a><br>'
+        for index, movie in enumerate(movie_list, start=1)
+    ]
+    return HttpResponse(movie_titles)
+
+def movie_detail(request, movie_id):
+    if movie_id==0 or movie_id > len(movie_list):
+        raise Http404
+    movie = movie_list[movie_id-1]
+    response_text = f'<h1>{movie["title"]}</h1> <p>관객 수 : {movie["viewers"]}</p>'
+    return HttpResponse(response_text)
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", index, name="index"),
     path("book_list/", book_list, name="book_list"),
     path("book_list/<int:num>/", book, name="book"),
-    path("language/<str:lang>/", language, name="language"),
+    path("language/python/", python),       # 얘는 language route 뒤에 호출되면 language 화면에서 <h1>태그로 호출되고 무시된다
+    path("language/<str:lang>/", language, name="language"),    # 그래서 보통 str로 인자값을 넣진 않음
+    path("movie/", movies, name="movies"),
+    path("movie/<int:movie_id>/", movie_detail),
 ]
